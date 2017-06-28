8e06209291a7ccf6edafd37344830e6c0b69de03
Chris Hitchcott <hitchcott@gmail.com>
2017-06-29 05:21:05 +0900

    add disableAdvanced option to tx modal

--- src/components/transactions/transaction_modal_form.jsx
+++ src/components/transactions/transaction_modal_form.jsx
@@ -8,42 +8,46 @@ export default class TransactionModalForm extends Component {
     formChange: PropTypes.func.isRequired,
     formData: PropTypes.object.isRequired,
     form: PropTypes.func,
+    disableAdvanced: PropTypes.bool,
     renderForm: PropTypes.func,
   }
   static defaultProps = {
     form: undefined,
     renderForm: undefined,
+    disableAdvanced: false,
   }
   render() {
-    const { renderForm, form: FormComponent, formChange, formData } = this.props;
+    const { disableAdvanced, renderForm, form: FormComponent, formChange, formData } = this.props;
     return (
       <div>
         {(renderForm && renderForm(this.props)) || (FormComponent && <FormComponent {...this.props} />)}
-        <Advanced>
-          <Form.Group widths="equal">
-            <FormField
-              type="number"
-              placeholder="e.g. `21000`"
-              label="Gas"
-              name="gas"
-              {...{ formChange, formData }}
-            />
-            <FormField
-              type="number"
-              placeholder="e.g. `20000000000`"
-              label="Gas Price"
-              name="gasPrice"
-              {...{ formChange, formData }}
-            />
-            <FormField
-              type="number"
-              placeholder="Leave empty to auto-detect"
-              label="Nonce"
-              name="nonce"
-              {...{ formChange, formData }}
-            />
-          </Form.Group>
-        </Advanced>
+        {!disableAdvanced &&
+          <Advanced>
+            <Form.Group widths="equal">
+              <FormField
+                type="number"
+                placeholder="e.g. `21000`"
+                label="Gas"
+                name="gas"
+                {...{ formChange, formData }}
+              />
+              <FormField
+                type="number"
+                placeholder="e.g. `20000000000`"
+                label="Gas Price"
+                name="gasPrice"
+                {...{ formChange, formData }}
+              />
+              <FormField
+                type="number"
+                placeholder="Leave empty to auto-detect"
+                label="Nonce"
+                name="nonce"
+                {...{ formChange, formData }}
+              />
+            </Form.Group>
+          </Advanced>
+        }
       </div>
     );
   }
