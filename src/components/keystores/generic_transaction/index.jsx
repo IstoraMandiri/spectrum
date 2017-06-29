57db5de60ea52a84ea00e674a6067afa432bb4a7
Chris Hitchcott <hitchcott@gmail.com>
2017-06-29 20:57:23 +0900

    remove other signing methods (for now)

--- src/components/keystores/generic_transaction/index.jsx
+++ src/components/keystores/generic_transaction/index.jsx
@@ -7,7 +7,7 @@ import { getAddresses } from '~/selectors';
 import { toBigNumber } from '~/helpers/stringUtils';
 
 import TransactionModal from '~/components/transactions/transaction_modal';
-import MenuSystem from '~/components/common/menu_system';
+// import MenuSystem from '~/components/common/menu_system';
 
 import GenericTransactionForm from './generic_transaction_form';
 
@@ -22,18 +22,19 @@ class GenericTransaction extends Component {
           const value = toBigNumber(ethValue).shift(18);
           return web3.eth.sendTransaction({ ...rest, value });
         }}
-        renderForm={props => (
-          <MenuSystem
-            equalWidths
-            // TODO reset form data on navigate
-            tabs={[
-              { name: 'Generic Data', icon: 'list', component: <GenericTransactionForm {...props} /> },
-              { name: 'Contract Method', icon: 'file text outline', component: <p>TODO</p> },
-              { name: 'Raw Transaction', icon: 'code', component: <p>TODO</p> },
-              { name: 'Text Message', icon: 'pencil', component: <p>TODO</p> },
-            ]}
-          />
-        )}
+        renderForm={props => <GenericTransactionForm {...props} />}
+        //   TODO add additional signing methods
+        //   <MenuSystem
+        //     equalWidths
+        //     // TODO reset form data on navigate
+        //     tabs={[
+        //       { name: 'Generic Data', icon: 'list', component: <GenericTransactionForm {...props} /> },
+        //       { name: 'Contract Method', icon: 'file text outline', component: <p>TODO</p> },
+        //       { name: 'Raw Transaction', icon: 'code', component: <p>TODO</p> },
+        //       { name: 'Text Message', icon: 'pencil', component: <p>TODO</p> },
+        //     ]}
+        //   />
+        // )}
         trigger={
           <Button onClick={e => e.preventDefault()} basic icon="pencil" content="Sign" />
         }
