f6790eed232fde64c337dd9a64f05d7b2dee7380
Chris Hitchcott <hitchcott@gmail.com>
2017-07-10 16:47:42 +0800

    lint ci wdio config, attempt to build with node 8

--- wdio.ci.config.js
+++ wdio.ci.config.js
@@ -1,11 +1,13 @@
-var config = require('./wdio.development.config').config;
+/* eslint-disable no-console */
+
+const config = require('./wdio.development.config').config;
 
 exports.config = Object.assign({}, config, {
-	services: ['sauce'],
-	user: process.env.SAUCE_USERNAME,
-	key: process.env.SAUCE_ACCESS_KEY,
-	sauceConnect: true,
-	sauceConnectOpts: {
-		logger: function(message) { console.log("LOG: ", message)}
-	}
+  services: ['sauce'],
+  user: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  sauceConnect: true,
+  sauceConnectOpts: {
+    logger(message) { console.log('LOG: ', message); },
+  },
 });
