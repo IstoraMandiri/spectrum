304a616cfb552656b52e1179473a14dbc85e14ca
Chris Hitchcott <hitchcott@gmail.com>
2017-07-19 18:27:03 +0800

    add `handleWarp` for paingation menu (for fast forwarding), items per page selector for redux_paginator

--- src/components/common/pagination_menu.jsx
+++ src/components/common/pagination_menu.jsx
@@ -7,6 +7,7 @@ export default class PaginationMenu extends Component {
     itemsPerPage: PropTypes.number.isRequired,
     handleNavigate: PropTypes.func.isRequired,
     disabled: PropTypes.func,
+    handleWarp: PropTypes.func,
     backDisabled: PropTypes.func,
     forwardDisabled: PropTypes.func,
     total: PropTypes.oneOfType([PropTypes.number, PropTypes.object]),
@@ -17,22 +18,38 @@ export default class PaginationMenu extends Component {
     backDisabled: undefined,
     forwardDisabled: undefined,
     total: undefined,
+    handleWarp: undefined,
     renderCenter: undefined,
   }
   render() {
     const {
-      renderCenter, total, currentPage, disabled, itemsPerPage,
+      renderCenter, total, currentPage, disabled, itemsPerPage, handleWarp,
       handleNavigate, backDisabled, forwardDisabled, ...rest
     } = this.props;
     const firstItem = currentPage * itemsPerPage;
-    const lastItem = firstItem + itemsPerPage;
+    const lastPageItem = firstItem + itemsPerPage;
+    const lastItem = lastPageItem > total ? total : lastPageItem;
     const bDisabled = currentPage === 0 || (disabled && disabled()) || (backDisabled && backDisabled());
     const fDisabled = (total && lastItem >= total) || (disabled && disabled()) || (forwardDisabled && forwardDisabled());
+    const totalPages = total && Math.ceil(total / itemsPerPage);
     return (
       <Menu {...rest} pagination size="small">
+        {total &&
+          <Menu.Item
+            as="a"
+            name="fast backward"
+            disabled={bDisabled}
+            onClick={(e) => {
+              e.preventDefault();
+              if (!bDisabled) { handleWarp(1); }
+            }}
+          >
+            <Icon name="fast backward" />
+          </Menu.Item>
+        }
         <Menu.Item
           as="a"
-          name="back"
+          name="warpback"
           disabled={bDisabled}
           onClick={(e) => {
             e.preventDefault();
@@ -57,6 +74,19 @@ export default class PaginationMenu extends Component {
         >
           <Icon name="step forward" />
         </Menu.Item>
+        {total &&
+          <Menu.Item
+            as="a"
+            name="warpfroward"
+            disabled={fDisabled}
+            onClick={(e) => {
+              e.preventDefault();
+              if (!fDisabled) { handleWarp(totalPages); }
+            }}
+          >
+            <Icon name="fast forward" />
+          </Menu.Item>
+        }
       </Menu>
 
     );
