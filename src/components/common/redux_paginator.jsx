7da13b87d04f3c78b30118231f86efd1e0367916
Chris Hitchcott <hitchcott@gmail.com>
2017-07-20 20:48:11 +0800

    add success_header component

--- src/components/common/redux_paginator.jsx
+++ src/components/common/redux_paginator.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react';
-import { Dropdown, Dimmer, Input, Loader, Table } from 'semantic-ui-react';
+import { Dropdown, Dimmer, Loader, Table } from 'semantic-ui-react';
 
 import PaginationMenu from './pagination_menu';
 
