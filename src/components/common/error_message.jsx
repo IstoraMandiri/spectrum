2283eb5f92e8dbe4c11ec5ac8285118b5d58ead4
Chris Hitchcott <hitchcott@gmail.com>
2017-07-31 15:13:05 +0800

    add children option for error_message & render objects

--- src/components/common/error_message.jsx
+++ src/components/common/error_message.jsx
@@ -1,19 +1,29 @@
 import React, { PropTypes, Component } from 'react';
 import { Message } from 'semantic-ui-react';
 
+function parseContent(content) {
+  if (!content) { return null; }
+  if (typeof content === 'string') {
+    return content;
+  }
+  return <pre>{JSON.stringify(content, null, 2)}</pre>;
+}
 export default class ErrorMessage extends Component {
   static propTypes = {
-    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+    content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    children: PropTypes.func,
   };
+  static defaultProps = {
+    children: undefined,
+  }
   render() {
-    const { content } = this.props;
-    const message = typeof content === 'string' ? content : (content.message || `${content}`);
+    const { content, children } = this.props;
     return (
       <Message
         negative
         header="Oops, something went wrong..."
-        content={message}
         icon="warning sign"
+        content={children || parseContent(content)}
       />
     );
   }
