ac7fec027a6efe6b5525b7b7a5bbcbebd631b17d
Chris Hitchcott <hitchcott@gmail.com>
2017-07-31 15:12:30 +0800

    fix auto-reload

--- src/helpers/offlinePlugin.js
+++ src/helpers/offlinePlugin.js
@@ -3,14 +3,14 @@
 // TODO start a timer and auto-refresh if we're not
 if (process.env.NODE_ENV === 'production') {
   let autoReload = true;
-  setTimeout(() => { autoReload = false; }, 6000 * 10);
+  setTimeout(() => { autoReload = false; }, 10 * 1000); // 10 secs
   const OfflinePluginRuntime = require('offline-plugin/runtime');
   OfflinePluginRuntime.install({
     onUpdateReady: () => {
       OfflinePluginRuntime.applyUpdate();
     },
     onUpdated: () => {
-      if (!autoReload) {
+      if (autoReload) {
         return window.location.reload();
       }
       if (window.confirm('A new version of the app has been pushed. Would you like to reload?')) {
