38069ec978b75c3e17d73607e42dcfddac048bc2
Chris Hitchcott <hitchcott@gmail.com>
2017-07-31 15:12:30 +0800

    add auto-reload

--- src/helpers/offlinePlugin.js
+++ src/helpers/offlinePlugin.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-alert, global-require */
 
+// TODO start a timer and auto-refresh if we're not
 if (process.env.NODE_ENV === 'production') {
+  let autoReload = true;
+  setTimeout(() => { autoReload = false; }, 10 * 1000); // 10 secs
   const OfflinePluginRuntime = require('offline-plugin/runtime');
   OfflinePluginRuntime.install({
     onUpdateReady: () => {
       OfflinePluginRuntime.applyUpdate();
     },
     onUpdated: () => {
-      if (window.confirm('A new version of the app has been pushed. Would you like to reload?')) {
-        window.location.reload();
+      if (autoReload) {
+        return window.location.reload();
       }
+      if (window.confirm('A new version of the app has been pushed. Would you like to reload?')) {
+        return window.location.reload();
+      }
+      return null;
     },
   });
 }
