35fafb9d1647fbab635d60caed4f5af7debbc646
Chris Hitchcott <hitchcott@gmail.com>
2017-08-03 13:08:58 +0800

    reset config to defaults

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
-import Dapplet from '@digix/kyc-system';
-
 export default {
-  menuStyle: 'hamburger', // default, hamburger, hidden
-  dapplet: { // remove this for 'dapp store' mode
-    path: '/kyc',
-    name: 'Digix KYC',
-    icon: 'id card',
-    component: Dapplet,
-  },
+  menuStyle: 'default', // default, hamburger, hidden
 };
