fa05213a663305a409b50345ed500f9ed737c082
Chris Hitchcott <hitchcott@gmail.com>
2017-08-28 16:40:14 +0800

    add `noTruncate` option to tx visiualisation

--- src/components/common/tx_visualisation.jsx
+++ src/components/common/tx_visualisation.jsx
@@ -8,16 +8,16 @@ export default class TxVisualisation extends Component {
   static propTypes = {
     items: PropTypes.array.isRequired,
   }
-  renderItem({ header, item, icon, identicon, image, subheader, data, color, dataLink }, i) {
+  renderItem({ header, item, icon, identicon, image, subheader, data, color, dataLink, noTruncate }, i) {
     return (
       <Grid.Column textAlign="center" key={i}>
         <Header as="h2" color={color} icon style={{ maxWidth: '100%' }}>
           {identicon && <Image src={blockie(identicon)} style={{ height: '1em' }} className="icon" />}
           {image && <Image src={image} style={{ height: '1em' }} className="icon" />}
           {icon && <Icon name={icon} />}
-          <span className="truncated">{header}</span>
+          <span className={!noTruncate ? 'truncated' : undefined}>{header}</span>
           <Header.Subheader>
-            <b className="truncated">{subheader}</b>
+            <b className={!noTruncate ? 'truncated' : undefined}>{subheader}</b>
             <br />
             {dataLink ?
               <a className="truncated" rel="noopener noreferrer" target="_blank" href={dataLink}>{data}</a>
