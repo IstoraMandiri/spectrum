07a1d809a7d1efd6afd1c7cb1a2491328f60e995
Chris Hitchcott <hitchcott@gmail.com>
2017-08-28 13:11:27 +0800

    add identicon option for tx_overlay

--- src/components/common/tx_visualisation.jsx
+++ src/components/common/tx_visualisation.jsx
@@ -1,16 +1,20 @@
 import React, { PropTypes, Component } from 'react';
 
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Image, Grid, Icon, Header } from 'semantic-ui-react';
+
+import blockie from '~/helpers/blockie';
 
 export default class TxVisualisation extends Component {
   static propTypes = {
     items: PropTypes.array.isRequired,
   }
-  renderItem({ header, item, icon, subheader, data, color, dataLink }, i) {
+  renderItem({ header, item, icon, identicon, image, subheader, data, color, dataLink }, i) {
     return (
       <Grid.Column textAlign="center" key={i}>
         <Header as="h2" color={color} icon style={{ maxWidth: '100%' }}>
-          <Icon name={icon} />
+          {identicon && <Image src={blockie(identicon)} style={{ height: '1em' }} className="icon" />}
+          {image && <Image src={image} style={{ height: '1em' }} className="icon" />}
+          {icon && <Icon name={icon} />}
           <span className="truncated">{header}</span>
           <Header.Subheader>
             <b className="truncated">{subheader}</b>
