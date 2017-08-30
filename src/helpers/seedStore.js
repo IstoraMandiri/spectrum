210e29e66b66697f93ec80e5fa5d3bec231232bd
Chris Hitchcott <hitchcott@gmail.com>
2017-08-30 14:14:21 +0800

    clean up seed-store.js

--- src/helpers/seedStore.js
+++ src/helpers/seedStore.js
@@ -1,8 +1,7 @@
 import { createDefaultSession } from '~/actions/session';
 import { createKeystoreType } from '~/actions/keystoreType';
-import { createNetwork, deleteNetwork } from '~/actions/network';
-import { createToken, deleteToken } from '~/actions/token';
-import { updateAddress } from '~/actions/address';
+import { createNetwork } from '~/actions/network';
+import { createToken } from '~/actions/token';
 
 import { init as initWeb3 } from '~/helpers/web3';
 
@@ -23,32 +22,14 @@ const tokens = !availableNetworks ? DEFAULT_TOKENS : DEFAULT_TOKENS.filter(t => 
 export default function seedStore(store) {
   const state = store.getState();
   if (state.orm.Session.items.length === 0) {
-    console.log('restoring defaults');
-    // nothing to restore from, populate defaults...
+    // if we did not rehydrate
+    console.log('initializing defaults');
     DEFAULT_KEYSTORE_TYPES.forEach(keystoreType => store.dispatch(createKeystoreType(keystoreType)));
     networks.forEach(network => store.dispatch(createNetwork(network)));
     tokens.forEach(token => store.dispatch(createToken(token)));
     store.dispatch(createDefaultSession());
   } else {
-    // resolve rehydrated state
-    console.log('restoring previous');
-    // TODO make this not a hack?
-    // const addressNetworks = Object.values(state.orm.AddressNetworks.itemsById).reduce((o, val) => ({
-    //   ...o, [val.fromAddressId]: (o[val.fromAddressId] || []).concat(val.toNetworkId),
-    // }), {});
-    // const addressTokens = Object.values(state.orm.AddressTokens.itemsById).reduce((o, val) => ({
-    //   ...o, [val.fromAddressId]: (o[val.fromAddressId] || []).concat(val.toTokenId),
-    // }), {});
-    // const addressNetworkTokens = Object.keys(addressNetworks).concat(Object.keys(addressTokens)).reduce((o, val) => ({
-    //   ...o, [val]: { address: val, tokens: addressTokens[val] || [], networks: addressNetworks[val] || [] },
-    // }), {});
-    // store.getState().orm.Token.items.forEach(token => store.dispatch(deleteToken(token)));
-    // store.getState().orm.Network.items.forEach(network => store.dispatch(deleteNetwork(network)));
-    // networks.forEach(network => store.dispatch(createNetwork(network)));
-    // tokens.forEach(token => store.dispatch(createToken(token)));
-    // repopuplate accounts
-    // Object.values(addressNetworkTokens).forEach(data => store.dispatch(updateAddress(data)));
+    console.log('restoring previous session');
     initWeb3(store);
   }
-  // TODO set 'ready' state...
 }
